e26996decf8e12c99115f2589daf0d8877b00e60
infocyn <162898685+infocyn@users.noreply.github.com>
2025-07-29 21:29:34 -0700

    Delete utils directory

--- utils/fake_headers.py
+++
@@ -1,9 +0,0 @@
-import random
-
-USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)...",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)...",
-]
-
-def random_user_agent():
-    return random.choice(USER_AGENTS)
